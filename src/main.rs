33b2cb3b9f40603e6e3afdebdb92e47f6022cb5f
Gavin Pease <gavinpease@gmail.com>
2021-08-04 10:56:41 -0600

    Add structopt

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,50 @@
 use lg_webos_client::command::Command;
 use lg_webos_client::client::*;
+use std::sync::RwLock;
+use warp::{Filter, http};
+use lazy_static::lazy_static;
+use std::convert::Infallible;
+use std::time::Duration;
+use std::str::FromStr;
+use structopt::StructOpt;
+
+lazy_static! {
+    static ref CONFIG: RwLock<WebOsClientConfig> = RwLock::new(WebOsClientConfig::default());
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "cmd")]
+struct Opt {
+    #[structopt(name = "arg")]
+    argument: Argument,
+}
+
+#[derive(Debug,StructOpt)]
+enum Argument {
+    SetVol {
+        volume: i8
+    },
+    GetVol,
+}
+
+impl FromStr for Argument {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "setvol" => Ok(Argument::SetVol),
+            "getvol" => Ok(Argument::GetVol),
+            _ => { panic!("could not match") }
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
     env_logger::init();
-    let config = WebOsClientConfig::new("ws://10.4.1.51:3000/", true, "df483773b34d4919c29ba348728f5987");
-    let mut client = WebosClient::new(config).await.unwrap();
-    let resp = client.send_command(Command::GetChannelList).await.unwrap();
-    println!("Got response {:?}", resp.payload);
+
+    let config: WebOsClientConfig = confy::load_path("lgtv.conf").unwrap();
+    let client = WebosClient::new(config).await.unwrap();
+    let opt = Opt::from_args();
+    println!("{:?}", opt)
 }
